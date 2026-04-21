0badf3dfc24ef57a644dc99203e69723d66e2623
ambroiseguyot-del <ambroiseguyot@gmail.com>
2026-04-21 11:17:40 +0200

    middleware v2

--- middleware.ts
+++ middleware.ts
@@ -43,5 +43,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/app/:path*', '/comparaison/:path*', '/analyse/:path*'],
+  matcher: ['/app/:path*'],
 };
